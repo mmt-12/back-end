78c0d9a4dcbe70e04c4b08100fe53242859076a4
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-07-11 15:57:05 +0900

    config: init spring boot project and add initial dependencies

---
+++ memento/src/test/java/com/memento/server/MementoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memento.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MementoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
