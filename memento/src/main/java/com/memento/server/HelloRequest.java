dd1225e88e46c6d81f19b7fa27c1a607ce5984a8
Sunwoo Yu <dbtjsdn980818@gmail.com>
2025-07-16 20:16:59 +0900

    Feature/1 (#2)
    * config: init spring boot project and add initial dependencies
    
    * config: migrate application.properties to application.yml
    
    * config: restdocs setting

---
+++ memento/src/main/java/com/memento/server/HelloRequest.java
@@ -0,0 +1,17 @@
+package com.memento.server;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+
+@Builder
+public record HelloRequest(
+	@NotNull(message = "id는 필수입니다.")
+	Long id,
+	@Positive(message = "가격은 필수입니다.")
+	Integer price,
+	@NotBlank(message = "이름은 필수입니다.")
+	String name
+) {
+}
