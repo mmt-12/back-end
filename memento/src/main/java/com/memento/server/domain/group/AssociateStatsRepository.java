c2fe59bbee82bbee66e0b662bba91e2b2cb745ff
kjh-0523 <nemo00528@gmail.com>
2025-07-19 17:53:56 +0900

    rename : AssociateStatus -> AssociateStats 수정

--- memento/src/main/java/com/memento/server/domain/group/AssociateStatsRepository.java
+++ memento/src/main/java/com/memento/server/domain/group/AssociateStatsRepository.java
@@ -2,5 +2,5 @@ package com.memento.server.domain.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssociateStatusRepository extends JpaRepository<AssociateStatus, Long> {
+public interface AssociateStatsRepository extends JpaRepository<AssociateStats, Long> {
 }
