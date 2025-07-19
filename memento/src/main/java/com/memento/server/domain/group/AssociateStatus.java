173cb4c8184d06d4792f57d8c6b5f8b310fd7a1a
kjh-0523 <nemo00528@gmail.com>
2025-07-19 17:34:31 +0900

    feat : Associate status 추가

---
+++ memento/src/main/java/com/memento/server/domain/group/AssociateStatus.java
@@ -0,0 +1,80 @@
+package com.memento.server.domain.group;
+
+import static jakarta.persistence.ConstraintMode.NO_CONSTRAINT;
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.memento.server.domain.member.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "associate_status")
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+@Builder
+public class AssociateStatus {
+
+	@Id
+	@OneToOne(fetch = LAZY)
+	@JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(NO_CONSTRAINT))
+	private Member member;
+
+	@Column(name = "consecutive_attendance_days", nullable = false, columnDefinition = "INT DEFAULT 1")
+	private int consecutiveAttendanceDays = 1;
+
+	@CreatedDate
+	@Column(name = "last_attended_at", nullable = false, updatable = false)
+	private LocalDateTime lastAttendedAt;
+
+	@Column(name = "uploaded_reaction_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int uploadedReactionCount;
+
+	@Column(name = "used_reaction_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int usedReactionCount;
+
+	@Column(name = "guest_book_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int guestBookCount;
+
+	@Column(name = "uploaded_profile_image_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int uploadedProfileImageCount;
+
+	@Column(name = "registered_profile_image_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int registeredProfileImageCount;
+
+	@Column(name = "uploaded_post_image_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int uploadedPostImageCount;
+
+	@Column(name = "created_memory_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int createdMemoryCount;
+
+	@Column(name = "joined_memory_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int joinedMemoryCount;
+
+	@Column(name = "mbti_test_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int mbtiTestCount;
+
+	@Column(name = "f_mbti_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int fMbtiCount;
+
+	@Column(name = "t_mbti_count", nullable = false, columnDefinition = "INT DEFAULT 0")
+	private int tMbtiCount;
+}
