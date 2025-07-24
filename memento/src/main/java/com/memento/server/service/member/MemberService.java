1e3c6439d422303f797e2bcc09d9e45fe4f64b2a
SeongHwan <aiya0422@gmail.com>
2025-07-24 16:46:55 +0900

    feat: member 조회 로직 추가
    - kakaoId가 String 이라서 필드 타입 수정 (50자)
    - kakaoId로 조회하는 로직 추가

---
+++ memento/src/main/java/com/memento/server/service/member/MemberService.java
@@ -0,0 +1,23 @@
+package com.memento.server.service.member;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.memento.server.domain.member.Member;
+import com.memento.server.domain.member.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	public Optional<Member> findMemberWithKakaoId(String kakaoId) {
+		return memberRepository.findByKakaoId(kakaoId);
+	}
+}
