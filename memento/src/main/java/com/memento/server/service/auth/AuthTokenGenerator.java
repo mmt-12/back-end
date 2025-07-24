ae239292b371c72bd35dc90a02ce35554d191acc
SeongHwan <aiya0422@gmail.com>
2025-07-24 16:47:37 +0900

    feat: jwt 토큰 생성 로직 추가

---
+++ memento/src/main/java/com/memento/server/service/auth/AuthTokenGenerator.java
@@ -0,0 +1,30 @@
+package com.memento.server.service.auth;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.memento.server.controller.auth.AuthToken;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenGenerator {
+
+	private static final String BEARER_TYPE = "Bearer";
+	private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1시간
+	private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; // 7일
+	private final JwtTokenProvider jwtTokenProvider;
+
+	public AuthToken generate(String uid) {
+		long now = (new Date()).getTime();
+		Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+		Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+		String accessToken = jwtTokenProvider.accessTokenGenerate(uid, accessTokenExpiredAt);
+		String refreshToken = jwtTokenProvider.refreshTokenGenerate(refreshTokenExpiredAt);
+
+		return AuthToken.of(accessToken, refreshToken, BEARER_TYPE);
+	}
+}
