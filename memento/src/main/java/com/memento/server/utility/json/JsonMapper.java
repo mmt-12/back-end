f7a404761a92cb378d705d841324ee22e1c4adf1
SeongHwan <aiya0422@gmail.com>
2025-07-29 14:11:24 +0900

    refactor: 토큰 관련 개선
    - 토큰 파서 추가
    - 클래스명 수정
    - jwt 토큰 클래스 추가

--- memento/src/main/java/com/memento/server/utility/json/JsonMapper.java
+++ memento/src/main/java/com/memento/server/utility/json/JsonMapper.java
@@ -1,4 +1,4 @@
-package com.memento.server.utility;
+package com.memento.server.utility.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
