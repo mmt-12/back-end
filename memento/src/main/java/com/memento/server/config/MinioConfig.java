0832ac4a2eb61f25d5b06daef5026b7048f954f8
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-07-29 16:16:50 +0900

    feat: minio 업로드, 조회, 삭제 테스트 코드 추가

--- memento/src/main/java/com/memento/server/config/MinioConfig.java
+++ memento/src/main/java/com/memento/server/config/MinioConfig.java
@@ -1,47 +1,18 @@
 package com.memento.server.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Getter
-@Component
+@Configuration
 public class MinioConfig {
 
-	@Value("${minio.url}")
-	private String url;
-
-	@Value("${minio.access-key}")
-	private String accessKey;
-
-	@Value("${minio.secret-key}")
-	private String accessSecret;
-
-	@Value("${minio.bucket}")
-	private String bucket;
-
 	@Bean
-	public MinioClient minioClient() {
-		MinioClient minioClient = MinioClient.builder()
-			.endpoint(url)
-			.credentials(accessKey, accessSecret)
+	public MinioClient minioClient(MinioProperties properties) {
+		return MinioClient.builder()
+			.endpoint(properties.getUrl())
+			.credentials(properties.getAccessKey(), properties.getSecretKey())
 			.build();
-		try {
-			boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
-			if (!found) {
-				minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
-			}
-		} catch (Exception e) {
-			log.warn("bucketError found : {}", e.getMessage(), e);
-		}
-		return minioClient;
 	}
-
 }
