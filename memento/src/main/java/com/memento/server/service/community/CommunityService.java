041f28730e7e53469c54ff9699f15bb948da8199
SeongHwan <aiya0422@gmail.com>
2025-08-05 15:24:12 +0900

    refactor: repository 의존성 추가

--- memento/src/main/java/com/memento/server/service/community/CommunityService.java
+++ memento/src/main/java/com/memento/server/service/community/CommunityService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.memento.server.controller.community.CommunityListResponse.CommunityResponse;
+import com.memento.server.domain.community.CommunityRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CommunityService {
+
+	private final CommunityRepository repository;
 
 	public List<CommunityResponse> searchAll(Long memberId) {
 		return null;
