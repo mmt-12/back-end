688c1c2f8887736628ba45b6086b6586f2fb7753
SeongHwan <aiya0422@gmail.com>
2025-08-12 20:16:05 +0900

    test: 커뮤니티 목록 조회 테스트 작성

--- memento/src/test/java/com/memento/server/spring/api/service/member/MemberServiceTest.java
+++ memento/src/test/java/com/memento/server/spring/api/service/member/MemberServiceTest.java
@@ -13,9 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.memento.server.api.controller.member.dto.CommunityListResponse;
 import com.memento.server.api.controller.member.dto.MemberSignUpResponse;
+import com.memento.server.api.service.community.AssociateService;
 import com.memento.server.api.service.member.MemberService;
 import com.memento.server.common.exception.MementoException;
+import com.memento.server.domain.community.Associate;
+import com.memento.server.domain.community.AssociateRepository;
+import com.memento.server.domain.community.Community;
+import com.memento.server.domain.community.CommunityRepository;
 import com.memento.server.domain.member.Member;
 import com.memento.server.domain.member.MemberRepository;
 
@@ -27,7 +33,59 @@ class MemberServiceTest {
 	private MemberService memberService;
 
 	@Autowired
+	private AssociateService associateService;
+
+	@Autowired
 	private MemberRepository memberRepository;
+
+	@Autowired
+	private AssociateRepository associateRepository;
+
+	@Autowired
+	private CommunityRepository communityRepository;
+
+	@Test
+	@DisplayName("커뮤니티 목록을 조회한다.")
+	void searchAllMyCommunities() {
+		// given
+		Member member = memberRepository.save(Member.create("김가가", "hong@test.com", LocalDate.of(1990, 1, 1), 1001L));
+		Member member2 = memberRepository.save(Member.create("김나나", "muge@test.com", LocalDate.of(1990, 1, 1), 1002L));
+		Member member3 = memberRepository.save(Member.create("김다다", "muge@test.com", LocalDate.of(1990, 1, 1), 1003L));
+		Member member4 = memberRepository.save(Member.create("김라라", "muge@test.com", LocalDate.of(1990, 1, 1), 1004L));
+		Community community = communityRepository.save(Community.create("comm", member));
+		Community community2 = communityRepository.save(Community.create("comm2", member2));
+		Community community3 = communityRepository.save(Community.create("comm3", member3));
+		Community community4 = communityRepository.save(Community.create("comm4", member4));
+		Associate associate1 = associateRepository.save(Associate.create("가가", member, community));
+		Associate associate2 = associateRepository.save(Associate.create("나나", member, community2));
+		Associate associate3 = associateRepository.save(Associate.create("다다", member, community3));
+		Associate associate4 = associateRepository.save(Associate.create("라라", member, community4));
+
+		// when
+		CommunityListResponse response = associateService.searchAllMyAssociate(member.getId());
+
+		// then
+		assertThat(response.communities().size()).isEqualTo(4);
+	}
+
+	@Test
+	@DisplayName("어느 커뮤니티에도 가입하지 않고 커뮤니티 목록을 조회한다.")
+	void searchAllMyCommunities_empty() {
+		// given
+		Member member = memberRepository.save(Member.create("김가가", "hong@test.com", LocalDate.of(1990, 1, 1), 1001L));
+		Member member2 = memberRepository.save(Member.create("김나나", "muge@test.com", LocalDate.of(1990, 1, 1), 1002L));
+		Member member3 = memberRepository.save(Member.create("김다다", "muge@test.com", LocalDate.of(1990, 1, 1), 1003L));
+		Member member4 = memberRepository.save(Member.create("김라라", "muge@test.com", LocalDate.of(1990, 1, 1), 1004L));
+		Community community2 = communityRepository.save(Community.create("comm2", member2));
+		Community community3 = communityRepository.save(Community.create("comm3", member3));
+		Community community4 = communityRepository.save(Community.create("comm4", member4));
+
+		// when
+		CommunityListResponse response = associateService.searchAllMyAssociate(member.getId());
+
+		// then
+		assertThat(response.communities().size()).isEqualTo(0);
+	}
 
 	@Test
 	@DisplayName("회원 가입을 한다.")
