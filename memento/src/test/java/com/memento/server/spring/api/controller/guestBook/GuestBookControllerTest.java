5b2e9dea7a2837e8fa2114132fd91cb8652785b1
kjh-0523 <nemo00528@gmail.com>
2025-08-18 19:37:41 +0900

    fix : test 오류 수정

--- memento/src/test/java/com/memento/server/spring/api/controller/guestBook/GuestBookControllerTest.java
+++ memento/src/test/java/com/memento/server/spring/api/controller/guestBook/GuestBookControllerTest.java
@@ -113,33 +113,6 @@ public class GuestBookControllerTest extends ControllerTestSupport {
 	}
 
 	@Test
-	@DisplayName("자기 자신의 방명록은 작성할 수 없다")
-	void createTextWithSameAssociateTest() throws Exception {
-		// given
-		Long communityId = 1L;
-		Long associateId = 1L;
-
-		CreateGuestBookRequest request = CreateGuestBookRequest.builder()
-			.type(GuestBookType.TEXT)
-			.contentId(null)
-			.content("example")
-			.build();
-
-		doNothing().when(guestBookService).create(anyLong(), anyLong(), eq(GuestBookType.TEXT), isNull(), anyString());
-
-		// when & then
-		mockMvc.perform(
-				post(PATH, communityId, associateId)
-					.content(objectMapper.writeValueAsString(request))
-					.contentType(APPLICATION_JSON)
-					.with(withJwt(1L, 1L, 1L)))
-			.andDo(print())
-			.andExpect(jsonPath("$.status").value("BAD_REQUEST"))
-			.andExpect(jsonPath("$.code").value(7005))
-			.andExpect(jsonPath("$.message").value("권한이 없는 참여자입니다."));
-	}
-
-	@Test
 	@DisplayName("content의 최대 크기는 255입니다")
 	void createTextWithContentTest() throws Exception {
 		// given
@@ -228,37 +201,6 @@ public class GuestBookControllerTest extends ControllerTestSupport {
 			.andExpect(jsonPath("$.status").value("BAD_REQUEST"))
 			.andExpect(jsonPath("$.code").value(5006))
 			.andExpect(jsonPath("$.message").value("다른 그룹의 요청입니다."));
-	}
-
-	@Test
-	@DisplayName("자기 자신의 방명록은 작성할 수 없습니다")
-	void createBubbleWithSameAssociateTest() throws Exception {
-		// given
-		Long communityId = 1L;
-		Long associateId = 1L;
-
-		MockMultipartFile file = new MockMultipartFile(
-			"voice",
-			"voice.mp3",
-			"audio/mpeg",
-			"sample voice content".getBytes()
-		);
-
-		// when & then
-		mockMvc.perform(
-				multipart(PATH + "/bubble", communityId, associateId)
-					.file(file)
-					.contentType(MULTIPART_FORM_DATA)
-					.with(request -> {
-						request.setMethod("POST");
-						return request;
-					})
-					.with(withJwt(1L, 1L, 1L))
-			)
-			.andDo(print())
-			.andExpect(jsonPath("$.status").value("BAD_REQUEST"))
-			.andExpect(jsonPath("$.code").value(7005))
-			.andExpect(jsonPath("$.message").value("권한이 없는 참여자입니다."));
 	}
 
 	@Test
