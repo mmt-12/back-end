3e98e42eb58e231d27f2bf2323ab8b7c4023136f
SeongHwan <aiya0422@gmail.com>
2025-09-08 20:01:20 +0900

    docs: 기억 상세 조회 api 문서 추가

--- memento/src/test/java/com/memento/server/docs/memory/MemoryControllerDocsTest.java
+++ memento/src/test/java/com/memento/server/docs/memory/MemoryControllerDocsTest.java
@@ -14,6 +14,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.subsecti
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -25,7 +26,6 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.memento.server.api.controller.memory.MemoryController;
 import com.memento.server.api.controller.memory.dto.CreateUpdateMemoryRequest;
@@ -35,6 +35,7 @@ import com.memento.server.api.controller.memory.dto.ReadAllMemoryResponse;
 import com.memento.server.api.controller.memory.dto.ReadAllMemoryResponse.MemoryResponse;
 import com.memento.server.api.controller.memory.dto.ReadAllMemoryResponse.MemoryResponse.LocationResponse;
 import com.memento.server.api.controller.memory.dto.ReadAllMemoryResponse.MemoryResponse.PeriodResponse;
+import com.memento.server.api.controller.memory.dto.ReadMemoryResponse;
 import com.memento.server.api.service.memory.MemoryService;
 import com.memento.server.docs.RestDocsSupport;
 
@@ -50,8 +51,76 @@ public class MemoryControllerDocsTest extends RestDocsSupport {
 	}
 
 	@Test
-	@DisplayName("기억 조회")
+	@DisplayName("기억 상세 조회")
 	void read() throws Exception {
+		// given
+		setAuthentication(1L, 1L, 1L);
+		when(memoryService.read(any())).thenReturn(
+			ReadMemoryResponse.builder()
+				.id(1L)
+				.title("일평 mt")
+				.description("우리가 함께 마신 소주와 수영장 물을 추억하며")
+				.period(ReadMemoryResponse.PeriodResponse.builder()
+					.startTime(LocalDateTime.of(2025, 6, 20, 10, 30))
+					.endTime(LocalDateTime.of(2025, 6, 21, 12, 30))
+					.build())
+				.location(ReadMemoryResponse.LocationResponse.builder()
+					.latitude(36.34512323F)
+					.longitude(138.7712322F)
+					.code(16335)
+					.name("양평 서종풀팬션")
+					.address("경기도 양평시 양평군")
+					.build()
+				)
+				.memberAmount(22)
+				.pictureAmount(64)
+				.pictures(List.of(
+					"https://aws.s3.memento/1",
+					"https://aws.s3.memento/2",
+					"https://aws.s3.memento/3",
+					"https://aws.s3.memento/4",
+					"https://aws.s3.memento/5",
+					"https://aws.s3.memento/6",
+					"https://aws.s3.memento/7",
+					"https://aws.s3.memento/8",
+					"https://aws.s3.memento/9"
+				))
+				.build()
+		);
+
+		// when & then
+		mockMvc.perform(get(PATH + "/{memoryId}", 1L, 1L))
+			.andExpect(status().isOk())
+			.andDo(document("memory-read-test",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				pathParameters(
+					parameterWithName("communityId").description("그룹 아이디"),
+					parameterWithName("memoryId").description("기억 아이디")
+				),
+				responseFields(
+					fieldWithPath("id").description("기억 아이디"),
+					fieldWithPath("title").description("기억 이름"),
+					fieldWithPath("description").description("기억 설명"),
+					subsectionWithPath("period").description("기억 기간 정보"),
+					fieldWithPath("period.startTime").description("기억 시작 시간"),
+					fieldWithPath("period.endTime").description("기억 끝 시간"),
+					subsectionWithPath("location").description("기억 장소 정보"),
+					fieldWithPath("location.latitude").description("장소 - 위도"),
+					fieldWithPath("location.longitude").description("장소 - 경도"),
+					fieldWithPath("location.code").description("장소 우편번호"),
+					fieldWithPath("location.name").description("장소 이름"),
+					fieldWithPath("location.address").description("장소 주소"),
+					fieldWithPath("memberAmount").description("기억 참여자 수"),
+					fieldWithPath("pictureAmount").description("기억 사진 수"),
+					fieldWithPath("pictures").description("사진 url 목록")
+				)
+			));
+	}
+
+	@Test
+	@DisplayName("기억 목록 조회")
+	void readAll() throws Exception {
 		// given
 		setAuthentication(1L, 1L, 1L);
 		when(memoryService.readAll(any(), any())).thenReturn(
@@ -103,7 +172,7 @@ public class MemoryControllerDocsTest extends RestDocsSupport {
 				.param("startTime", "2025-08-06")
 				.param("endTime", "2025-08-06"))
 			.andExpect(status().isOk())
-			.andDo(document("memory-read-test",
+			.andDo(document("memory-read-all-test",
 				preprocessRequest(prettyPrint()),
 				preprocessResponse(prettyPrint()),
 				pathParameters(
@@ -258,12 +327,12 @@ public class MemoryControllerDocsTest extends RestDocsSupport {
 
 	@Test
 	@DisplayName("기억 삭제")
-	void delete() throws Exception {
+	void deleteMemory() throws Exception {
 		// given
 		setAuthentication(1L, 1L, 1L);
 
 		// when & then
-		mockMvc.perform(MockMvcRequestBuilders.delete(PATH + "/{memoryId}", 1L, 1L)
+		mockMvc.perform(delete(PATH + "/{memoryId}", 1L, 1L)
 				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andDo(document("memory-delete-test",
@@ -296,7 +365,7 @@ public class MemoryControllerDocsTest extends RestDocsSupport {
 		);
 
 		// when & then
-		mockMvc.perform(MockMvcRequestBuilders.get(PATH + "/{memoryId}", 1L, 1L))
+		mockMvc.perform(get(PATH + "/{memoryId}/images", 1L, 1L))
 			.andExpect(status().isOk())
 			.andDo(document("memory-download-test",
 				preprocessRequest(prettyPrint()),
