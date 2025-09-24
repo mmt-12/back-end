fe09056cf7cd0caece2f42021af8756214221a1f
wndgur2 <wndgur76@naver.com>
2025-09-25 00:09:29 +0900

    fix: associate의 emitter 사용

--- memento/src/main/java/com/memento/server/client/sse/SseEmitterRepository.java
+++ memento/src/main/java/com/memento/server/client/sse/SseEmitterRepository.java
@@ -11,6 +11,7 @@ public class SseEmitterRepository {
 	private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
 
 	public void save(Long associateId, SseEmitter emitter){
+		System.out.println("SseEmitterRepository save: " + associateId);
 		emitters.put(associateId, emitter);
 	}
 
