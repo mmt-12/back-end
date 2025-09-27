09875d701eda53101c359bd731a8daaf25444a51
kjh-0523 <nemo00528@gmail.com>
2025-09-27 16:04:48 +0900

    fix : schedule 설정 추가

--- memento/src/main/java/com/memento/server/MementoApplication.java
+++ memento/src/main/java/com/memento/server/MementoApplication.java
@@ -2,8 +2,10 @@ package com.memento.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MementoApplication {
 
 	public static void main(String[] args) {
