e850f024903faad75f400a63cc9f463ade06bd6c
SeongHwan <aiya0422@gmail.com>
2025-09-30 20:45:13 +0900

    feat: 기억 조회에 작성자 정보 추가

---
+++ memento/src/main/java/com/memento/server/api/service/memory/dto/Author.java
@@ -0,0 +1,19 @@
+package com.memento.server.api.service.memory.dto;
+
+import lombok.Builder;
+
+@Builder
+public record Author(
+	Long id,
+	String imageUrl,
+	String nickname,
+	Achievement achievement
+) {
+
+	@Builder
+	public record Achievement(
+		Long id,
+		String name
+	) {
+	}
+}
