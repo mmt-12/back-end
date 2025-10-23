4ad4d93a0ec538300fd479736766957144d7cc73
SeongHwan <aiya0422@gmail.com>
2025-10-23 15:50:43 +0900

    refactor: auth controller dto

--- memento/src/main/java/com/memento/server/api/controller/auth/dto/AuthGuestResponse.java
+++ memento/src/main/java/com/memento/server/api/controller/auth/dto/AuthGuestResponse.java
@@ -2,12 +2,21 @@ package com.memento.server.api.controller.auth.dto;
 
 import com.memento.server.api.service.auth.jwt.JwtToken;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 
-@Builder
+@Builder(access = AccessLevel.PROTECTED)
 public record AuthGuestResponse(
 	Long kakaoId,
 	String email,
 	JwtToken token
 ) implements AuthResponse {
+
+	public static AuthGuestResponse of(Long kakaoId, String email, JwtToken jwtToken) {
+		return AuthGuestResponse.builder()
+			.kakaoId(kakaoId)
+			.email(email)
+			.token(jwtToken)
+			.build();
+	}
 }
--- memento/src/main/java/com/memento/server/api/service/auth/AuthService.java
+++ memento/src/main/java/com/memento/server/api/service/auth/AuthService.java
@@ -10,7 +10,6 @@ import com.memento.server.api.controller.auth.dto.AuthGuestResponse;
 import com.memento.server.api.controller.auth.dto.AuthMemberResponse;
 import com.memento.server.api.controller.auth.dto.AuthResponse;
 import com.memento.server.api.controller.auth.dto.TokenRefreshRequest;
-import com.memento.server.api.service.achievement.AchievementEventPublisher;
 import com.memento.server.api.service.auth.jwt.JwtToken;
 import com.memento.server.api.service.auth.jwt.JwtTokenProvider;
 import com.memento.server.api.service.auth.jwt.MemberClaim;
@@ -21,7 +20,6 @@ import com.memento.server.client.oauth.KakaoClient;
 import com.memento.server.common.exception.MementoException;
 import com.memento.server.domain.community.Associate;
 import com.memento.server.domain.community.AssociateRepository;
-import com.memento.server.domain.community.SignInAchievementEvent;
 import com.memento.server.domain.member.Member;
 import com.memento.server.domain.member.MemberRepository;
 
@@ -37,7 +35,6 @@ public class AuthService {
 	private final KakaoOpenIdDecoder kakaoOpenIdDecoder;
 	private final AssociateRepository associateRepository;
 	private final MemberRepository memberRepository;
-	private final AchievementEventPublisher achievementEventPublisher;
 
 	public String getAuthUrl() {
 		return kakaoClient.getAuthUrl();
@@ -62,11 +59,7 @@ public class AuthService {
 					.build();
 				JwtToken token = jwtTokenProvider.createToken(memberClaim);
 
-				return AuthMemberResponse.builder()
-					.memberId(member.getId())
-					.name(member.getName())
-					.token(token)
-					.build();
+				return AuthMemberResponse.of(member.getId(), member.getName(), token);
 			})
 			.orElseGet(() -> {
 				MemberClaim memberClaim = MemberClaim.builder()
@@ -75,11 +68,7 @@ public class AuthService {
 					.build();
 				JwtToken token = jwtTokenProvider.createTempToken(memberClaim);
 
-				return AuthGuestResponse.builder()
-					.kakaoId(kakaoId)
-					.email(openIdPayload.email())
-					.token(token)
-					.build();
+				return AuthGuestResponse.of(kakaoId, openIdPayload.email(), token);
 			});
 	}
 
@@ -96,10 +85,6 @@ public class AuthService {
 
 		MemberClaim memberClaim = MemberClaim.from(member, associate);
 		JwtToken token = jwtTokenProvider.createToken(memberClaim);
-		return AuthMemberResponse.builder()
-			.memberId(member.getId())
-			.name(member.getName())
-			.token(token)
-			.build();
+		return AuthMemberResponse.of(member.getId(), member.getName(), token);
 	}
 }
