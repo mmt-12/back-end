6b090c51d7f1a703dd4db0dc8141f24edcefe907
yusunwoo <swyu@nkia.co.kr>
2026-01-02 15:22:19 +0900

    feature: MemberRepository에 existsByEmail 메서드 추가

--- memento/src/main/java/com/memento/server/domain/member/MemberRepository.java
+++ memento/src/main/java/com/memento/server/domain/member/MemberRepository.java
@@ -16,4 +16,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByEmail(String email);
 
 	Optional<Member> findByBirthday(LocalDate birthday);
+
+  Boolean existsByEmail(String email);
 }
